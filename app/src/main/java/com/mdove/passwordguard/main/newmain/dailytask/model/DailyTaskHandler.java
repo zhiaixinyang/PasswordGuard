26f87e90fc7651dc4615e830a7a464b7d0849604
zhaojing <1024624013@qq.com>
2018-05-26 12:13:49 +0800

    fix something

---
+++ app/src/main/java/com/mdove/passwordguard/main/newmain/dailytask/model/DailyTaskHandler.java
@@ -0,0 +1,51 @@
+package com.mdove.passwordguard.task.model.handle;
+
+import android.view.View;
+import android.widget.EditText;
+
+import com.mdove.passwordguard.R;
+import com.mdove.passwordguard.task.model.SelfTaskModelVM;
+import com.mdove.passwordguard.task.presenter.AllSelfTaskPresenter;
+import com.mdove.passwordguard.task.presenter.SelfTaskPresenter;
+import com.mdove.passwordguard.utils.ToastHelper;
+
+/**
+ * Created by MDove on 2018/3/27.
+ */
+
+public class AllSelfTaskHandler {
+    private AllSelfTaskPresenter mPresenter;
+
+    public AllSelfTaskHandler(AllSelfTaskPresenter presenter) {
+        mPresenter = presenter;
+    }
+
+    public void onClickTaskSuc(SelfTaskModelVM vm) {
+        mPresenter.onClickTaskSuc(vm);
+    }
+
+    public void onClickSee(SelfTaskModelVM vm) {
+        mPresenter.onClickSee(vm);
+    }
+
+    public void onClickBtnEdit(View editText, SelfTaskModelVM vm) {
+        boolean isChange = true;
+        if (vm.mTask.get().equals(vm.mSelfTaskModel.mSelfTask.mTask)) {
+            isChange = false;
+        }
+        editText.clearFocus();
+        mPresenter.onClickBtnEdit(vm, isChange);
+    }
+
+    public void onClickDelete(SelfTaskModelVM vm) {
+        mPresenter.onClickDelete(vm);
+    }
+
+    public void onClickPriority(SelfTaskModelVM vm) {
+        mPresenter.onClickPriority(vm);
+    }
+
+    public void onClickCopy(SelfTaskModelVM vm) {
+        mPresenter.onClickCopy(vm);
+    }
+}
---
+++ app/src/main/java/com/mdove/passwordguard/main/newmain/dailytask/model/DailyTaskModelVM.java
@@ -0,0 +1,40 @@
+package com.mdove.passwordguard.task.model;
+
+import android.databinding.ObservableField;
+import android.text.Editable;
+import android.text.TextWatcher;
+
+import com.mdove.passwordguard.base.SimpleTextWatcher;
+import com.mdove.passwordguard.utils.DateUtils;
+
+/**
+ * Created by MDove on 2018/3/25.
+ */
+
+public class SelfTaskModelVM {
+    public ObservableField<String> mTime = new ObservableField<>();
+    public ObservableField<String> mTask = new ObservableField<>();
+    public ObservableField<Boolean> mIsSuc = new ObservableField<>();
+    public ObservableField<Boolean> mIsSee = new ObservableField<>();
+    public ObservableField<Integer> mPriority = new ObservableField<>();
+    public SelfTaskModel mSelfTaskModel;
+    public int mPosition;
+
+    public SelfTaskModelVM(SelfTaskModel selfTaskModel, int position) {
+        mTask.set(selfTaskModel.mTask);
+        mTime.set(DateUtils.getDateChinese(selfTaskModel.mTime));
+        mIsSuc.set(selfTaskModel.mIsSuc);
+        mIsSee.set(selfTaskModel.mIsSee);
+        mPriority.set(selfTaskModel.mPriority);
+        mSelfTaskModel = selfTaskModel;
+        mPosition = position;
+    }
+
+    public TextWatcher textWatcher = new SimpleTextWatcher() {
+        @Override
+        public void afterTextChanged(Editable s) {
+            mTask.set(s.toString());
+            mSelfTaskModel.mTask = s.toString();
+        }
+    };
+}
